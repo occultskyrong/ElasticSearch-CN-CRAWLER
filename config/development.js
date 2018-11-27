a17ada6b3a3b1de4200caf3756a0528f49cd801f
ZhangRZ <occultskyrong@163.com>
2018-11-27 18:21:50 +0800

    feat(server): add server config

---
+++ config/development.js
@@ -0,0 +1,13 @@
+module.exports = {
+  server: {
+    host: 'http://localhost',
+    port: '3918',
+  },
+  target: {
+    host: 'https://elasticsearch.cn/',
+    url: {
+      question: 'sort_type-new__day-0__is_recommend-0__page-',
+      article: 'article/page-',
+    },
+  },
+};
